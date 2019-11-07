d58e2144d43c0075de49cfaf014b081807597f18
João Victor Girard <joao.nunes@corp.globo.com>
2019-11-07 10:43:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/m3_insecure_communication/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.m3_insecure_communication.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
